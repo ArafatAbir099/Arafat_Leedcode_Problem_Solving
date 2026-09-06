36d31c5e8e63d90f59d30a83bd1cc360c7eb872e
Md. Arafat Mahamud <253-15-830@diu.edu.bd>
2026-09-07 01:37:58 +0600

    Time: 0 ms (100%), Space: 19.9 MB (23.74%) - LeetHub

--- 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
@@ -9,3 +9,21 @@ public:
       return letters[0];
     }
 };
+
+/*START
+
+Take letters and target
+
+FOR each character in letters
+
+    IF character > target
+
+        RETURN character
+
+END FOR
+
+RETURN letters[0]
+
+END 
+
+*/
