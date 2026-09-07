55e5ebd550421170fec468594ff9cf65944a4601
Md. Arafat Mahamud <253-15-830@diu.edu.bd>
2026-09-07 17:29:34 +0600

    Time: 7 ms (2.77%), Space: 50.3 MB (33.77%) - LeetHub

---
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int current = 0;
+        int maximum = 0;
+
+        for(int i = 0; i < nums.size(); i++){
+
+            if(nums[i] == 1){
+                current = current + 1;
+
+                if(current > maximum){
+                    maximum = current;
+                }
+            }
+            else{
+                current = 0;
+            }
+        }
+
+        return maximum;
+    }
+};
