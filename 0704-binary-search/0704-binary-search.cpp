374d8e650c3ed1b27b7e8516e513a0e59aff9e20
Md. Arafat Mahamud <253-15-830@diu.edu.bd>
2026-09-13 17:41:28 +0600

    Time: 0 ms (100%), Space: 31.3 MB (81.13%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+       int left,right,mid;
+       left=0;
+       right=nums.size()-1;
+       while(left <= right){
+        mid=(left+right)/2;
+       if(target==nums[mid]){
+        return mid;
+       }else if(target > nums[mid]){
+        left=mid +1 ;
+       } else {
+        right=mid-1;
+       }
+    }
+       return -1;
+
+
+    }
+};
